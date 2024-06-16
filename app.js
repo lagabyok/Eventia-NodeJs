8a14d9ed319e796b3b130e7ab8b591dc4a5f9baa
Germán Pablo Gonzalez <germancai@hotmail.com>
2024-06-15 21:27:26 -0300

    agregando rutas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000 || process.env.PORT || 8000;
+const path = require('path');
+const port = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
@@ -8,6 +9,21 @@ app.use(express.static('public'));
 //     res.send("<h1>Eventia on Node JS</h1>");
 // })
 
+app.get('/contacto', (req, res) => {
+    res.sendFile(path.resolve(__dirname  + `/public/contacto.html`));
+})
+
+app.get('/nosotros', (req, res) => {
+    res.sendFile(path.resolve(__dirname + `/public/nosotros.html`));
+})
+
+app.get('/eventos', (req, res) => {
+    res.sendFile(path.resolve(__dirname + `/public/eventos.html`));
+})
+
+app.use((req, res, next) => {
+    res.status(400).send('<h1>Recurso no encontrado!</h1>')
+});
 
 app.listen(port, () => {
     console.log(`Servidor arriba en el puerto http://localhost:${port}`);
