c3358d0e98137b53fb403ebf4a9f9b10b0be2595
Germán Pablo Gonzalez <germancai@hotmail.com>
2024-06-23 18:57:35 -0300

    Modificacion ruta

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const rutas = require('../src/routes/index.routes.js');
+const indexRoutes = require('../src/routes/index.routes.js');
 const port = process.env.PORT || 8000;
 
 
 app.use(express.static('public'));
 
-app.use('/', rutas);
+app.use('/', indexRoutes);
+
 
 app.listen(port, () => {
     console.log(`Servidor arriba en el puerto http://localhost:${port}`);
